ed43def97cc50bb4aee0beaddb25c3246f8833ca
sjcasella <sjcasella@icloud.com>
2016-07-05 21:29:58 -0400

    Ticker box added

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -18,6 +18,7 @@ render: function() {
       <div id="quoteDiv">
         {this.renderQuotes()}
         {this.smallBar()}
+        {this.renderList()}
       </div>
       <div id="secondary-bg">
         {this.renderChoice()}
@@ -39,6 +40,48 @@ renderChoice: function(){
       </div>
   )
   }
+},
+renderList: function() {
+    return (<div className="dropdown">
+              <button className="dropbtn">Tickers...</button>
+                <div className="dropdown-content">
+                  {this.completeList()}
+                </div>
+              </div>)
+},
+completeList: function() {
+  liArray = []
+  arr = ['ABT','ABBV','ACN','ATVI','AYI','ADBE','AAP','AES','AET','AMG','AFL','A','GAS','APD','AKAM','ALK','AA','ALXN',
+  'ALLE','AGN','ADS','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP','AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH',
+  'APC','ADI','ANTM','AON','APA','AIV','AAPL','AMAT','ADM','AJG','AIZ','T','ADSK','ADP','AN','AZO','AVGO','AVB','AVY','BHI',
+  'BLL','BAC','BCR','BAX','BBT','BDX','BBBY','BRK-B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX','BMY','BF-B','CHRW','CA',
+  'CVC','COG','CPB','COF','CAH','KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW','CHK','CVX','CMG','CB',
+  'CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG','CTXS','CME','CMS','COH','CTSH','CL','CPGX','CMCSA','CMA','CAG','CXO','COP','ED',
+  'STZ','GLW','COST','CCI','CSRA','CSX','CMI','CVS','DHI','DHR','DRI','DVA','DE','DLPH','DAL','XRAY','DVN','DO','DLR','DFS','DISCA',
+  'DISCK','DG','DLTR','D','DOV','DOW','DPS','DTE','DD','DUK','DNB','ETFC','EMN','ETN','EBAY','ECL','EIX','EW','EA','EMC','EMR','ENDP',
+  'ETR','EOG','EQT','EFX','EQIX','EQR','ESS','EL','ES','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST','FRT','FDX','FIS',
+  'FITB','FSLR','FE','FISV','FLIR','FLS','FLR','FMC','FTI','FL','F','BEN','FCX','FTR','GPS','GRMN','GD','GE','GGP','GIS','GM','GPC',
+  'GILD','GPN','GS','GT','GWW','HAL','HBI','HOG','HAR','HRS','HIG','HAS','HCA','HCP','HP','HSIC','HES','HPE','HOLX','HD','HON','HRL',
+  'HST','HPQ','HUM','HBAN','ITW','ILMN','IR','INTC','ICE','IBM','IP','IPG','IFF','INTU','ISRG','IVZ','IRM','JBHT','JEC','JNJ','JCI','JPM',
+  'JNPR','KSU','K','KEY','KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LM','LEG','LEN','LUK','LVLT','LLY','LNC','LLTC',
+  'LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MMM','MNK','MRO','MPC','MAR','MMC','MLM','MAS','MA','MAT','MKC','MCD','MCK','MJN','MDT','MRK','MET',
+  'KORS','MCHP','MU','MSFT','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MSI','MUR','MYL','NDAQ','NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM',
+  'NWSA','NWS','NEE','NLSN','NKE','NI','NBL','JWN','NSC','NTRS','NOC','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE','ORCL','OI','PCAR','PH',
+  'PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE','PCG','PM','PSX','PNW','PXD','PBI','PNC','RL','PPG','PPL','PX','PCLN','PFG',
+  'PG','PGR','PLD','PRU','PEG','PSA','PHM','PVH','QRVO','QCOM','PWR','DGX','RRC','RTN','O','RHT','REGN','RF','RSG','RAI','RHI','ROK','COL',
+  'ROP','ROST','RCL','R','SPGI','CRM','SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SJM','SNA','SO','LUV','SWN','SE',
+  'STJ','SWK','SPLS','SBUX','HOT','STT','SRCL','SYK','STI','SYMC','SYF','SYY','TROW','TGT','TEL','TE','TGNA','TDC','TSO','TXN','TXT','BK',
+  'CLX','KO','HSY','MOS','TRV','DIS','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','RIG','TRIP','FOXA','FOX','TYC','TSN','USB','UDR','ULTA',
+  'UA','UNP','UAL','UNH','UPS','URI','UTX','UHS','UNM','URBN','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO','VMC','WMT',
+  'WBA','WM','WAT','WFC','HCN','WDC','WU','WRK','WY','WHR','WFM','WMB','WLTW','WEC','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YHOO','YUM',
+  'ZBH','ZION','ZTS']
+
+  for (ind=0; ind < arr.length; ind++) {
+    liArray.push(<a key={arr[ind]} onClick={this.listClick}>{arr[ind]}</a>)
+  }
+
+  return liArray
+
 },
 smallBar: function() {
   if (this.showHome == false) {
@@ -62,6 +105,11 @@ quoteClick: function(event) {
   this.showHome = true
   this.forceUpdate()
   this.buttonPressed(event.target.value)
+},
+listClick: function(event) {
+  this.showHome = true
+  this.forceUpdate()
+  this.buttonPressed(event.target.text)
 },
 sButtonPressed: function() {
   this.showHome = false
@@ -163,7 +211,7 @@ formatQuote: function(sym,change,name) {
 
   if (this.showHome == true) {
     finalStyle = {
-      fontSize: '16px'
+      fontSize: '18px'
     }
   } else {
     finalStyle = {
@@ -174,9 +222,9 @@ formatQuote: function(sym,change,name) {
 
   if (change.charAt(0) == '-') {
     if (sym == "S&P 500" || sym == "DOW" || sym == "NASDAQ") {
-      return <div><p id="redChange">{tempChg+"%"}</p>{"\u00a0"}{"\u00a0"}<a onClick={this.quoteClick} value={sym} id="quotePar" style={finalStyle}>{sym}</a></div>
-    } else {
       return <div><p id="redChange">{tempChg+"%"}</p>{"\u00a0"}{"\u00a0"}<p value={sym} id="quotePar" style={finalStyle}>{sym}</p></div>
+    } else {
+      return <div><p id="redChange">{tempChg+"%"}</p>{"\u00a0"}{"\u00a0"}<a onClick={this.quoteClick} value={sym} id="quotePar" style={finalStyle}>{sym}</a></div>
     }
   } else {
     if (sym == "S&P 500" || sym == "DOW" || sym == "NASDAQ") {
